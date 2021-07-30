bf05be7058352c016fd66e373b8f0cb2d5796635
OscarMugendi <87582596+OscarMugendi@users.noreply.github.com>
2021-07-30 10:35:18 +0300

    missing src file

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeService } from './home/home.service';
+import { RepoComponent } from './repo/repo.component';
+
+@NgModule({
+  declarations: [	
+    AppComponent,
+    HomeComponent,
+    RepoComponent
+   ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule,
+  ],
+  providers: [HomeService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
